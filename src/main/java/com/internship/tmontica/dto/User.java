31d3defea8569a991ce2ca741563807a010f60a3
sam <vndtjd1217@naver.com>
2019-07-17 11:52:17 +0900

    add mybatis camelcase config to properties file

--- src/main/java/com/internship/tmontica/dto/User.java
+++ src/main/java/com/internship/tmontica/dto/User.java
@@ -2,8 +2,18 @@ package com.internship.tmontica.dto;
 
 import lombok.Data;
 
+import java.sql.Date;
+
 @Data
 public class User {
 
+    private String id;
+    private String name;
+    private String email;
+    private Date birthDate;
+    private String password;
+    private String role;
+    private Date createdDate;
+    private int point;
 
 }
