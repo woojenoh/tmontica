83c3549dd572e3c4abad662a8f335cb1b50510c7
WOOJE <woojenoh@gmail.com>
2019-07-18 22:04:09 +0900

    SigninForm 인풋 핸들링

--- front-end/src/service/components/SigninForm/index.tsx
+++ front-end/src/service/components/SigninForm/index.tsx
@@ -3,18 +3,43 @@ import "./styles.scss";
 
 export interface ISigninFormProps {}
 
-export interface ISigninFormState {}
+export interface ISigninFormState {
+  id: string;
+  password: string;
+}
 
 class SigninForm extends React.Component<ISigninFormProps, ISigninFormState> {
-  public render() {
+  state = {
+    id: "",
+    password: ""
+  };
+
+  handleInputChange = (e: React.FormEvent<HTMLInputElement>) => {
+    this.setState({
+      [e.currentTarget.name]: e.currentTarget.value
+    } as { [K in keyof ISigninFormState]: ISigninFormState[K] });
+  };
+
+  render() {
+    const { handleInputChange } = this;
+
     return (
       <form className="signin__form">
-        <input type="text" name="id" className="input signin__input" placeholder="아이디" />
+        <input
+          type="text"
+          name="id"
+          className="input signin__input"
+          placeholder="아이디"
+          onChange={e => handleInputChange(e)}
+          required
+        />
         <input
           type="password"
           name="password"
           className="input signin__input"
           placeholder="비밀번호"
+          onChange={e => handleInputChange(e)}
+          required
         />
         <span className="signin__find">아이디/비밀번호 찾기</span>
         <div className="signin__button-container">
