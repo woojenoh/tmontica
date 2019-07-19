996a21d9bd633e27e146a7d01d8d89296beb1009
김현정(RosyTF) <khj1259@tmon.co.kr>
2019-07-19 17:56:55 +0900

    cros 문제 해결(임시)

--- src/main/java/com/internship/tmontica/controller/MenuController.java
+++ src/main/java/com/internship/tmontica/controller/MenuController.java
@@ -4,10 +4,7 @@ package com.internship.tmontica.controller;
 import com.internship.tmontica.dto.response.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
