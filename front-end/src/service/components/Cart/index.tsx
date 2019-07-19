da2017e0c7a153fd70751d9b11fec87741f3bb5f
노우제 <woojenoh@gmail.com>
2019-07-20 00:55:19 +0900

    Cart 더미 데이터 적용, CartItem 셀렉트 핸들링

--- front-end/src/service/components/Cart/index.tsx
+++ front-end/src/service/components/Cart/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import CartItem from "../CartItem";
+import { numberCommaRegex } from "../../../utils";
 import "./styles.scss";
 
 export interface ICartProps {
@@ -10,8 +11,36 @@ export interface ICartProps {
 export interface ICartState {}
 
 export default class Cart extends React.Component<ICartProps, ICartState> {
+  tempData = {
+    size: 2,
+    totalPrice: 2500,
+    menus: [
+      {
+        cardId: 10,
+        menuId: 2,
+        menuNameEng: "americano",
+        menuNameKo: "아메리카노",
+        option: "HOT/샷추가(1개)/사이즈업",
+        quantity: 1,
+        price: 1000,
+        stock: 100
+      },
+      {
+        cardId: 11,
+        menuId: 3,
+        menuNameEng: "caffelatte",
+        menuNameKo: "카페라떼",
+        option: "HOT/샷추가(1개)/사이즈업",
+        quantity: 1,
+        price: 1500,
+        stock: 50
+      }
+    ]
+  };
+
   render() {
     const { isCartOpen, handleCartClose } = this.props;
+    const { tempData } = this;
 
     return (
       <section className={isCartOpen ? "cart" : "cart cart--close"}>
@@ -29,12 +58,22 @@ export default class Cart extends React.Component<ICartProps, ICartState> {
             <span className="cart__delete">삭제</span>
           </ul>
           <ul className="cart__items">
-            <CartItem />
+            {tempData.menus.map((m, index) => {
+              return (
+                <CartItem
+                  key={index}
+                  name={m.menuNameKo}
+                  price={m.price}
+                  option={m.option}
+                  quantity={m.quantity}
+                />
+              );
+            })}
           </ul>
           <div className="cart__bottom">
             <div className="cart__total">
-              <span className="cart__total-quantity">총 2개</span>
-              <span className="cart__total-price">4,000원</span>
+              <span className="cart__total-quantity">총 {tempData.size}개</span>
+              <span className="cart__total-price">{numberCommaRegex(tempData.totalPrice)}원</span>
             </div>
             <button className="button cart__button">결제 및 주문하기</button>
           </div>
--- front-end/src/service/components/CartItem/index.tsx
+++ front-end/src/service/components/CartItem/index.tsx
@@ -1,21 +1,72 @@
 import * as React from "react";
+import { numberCommaRegex } from "../../../utils";
 import "./styles.scss";
 
-export interface ICartItemProps {}
+export interface ICartItemProps {
+  name: string;
+  price: number;
+  option: string;
+  quantity: number;
+}
 
-function CartItem(props: ICartItemProps) {
-  return (
-    <li className="cart__item">
-      <img src="/img/coffee-sample.png" alt="Coffee Sample" className="cart__item-img" />
-      <div className="cart__item-info">
-        <span className="cart__item-name">아메리카노 - 1,000원</span>
-        <span className="cart__item-option">HOT/샷추가(1개)/시럽추가(1개)/사이즈추가</span>
-      </div>
-      <div className="cart__item-quantity">
-        <input type="number" className="cart__item-quantity-input" value="1" />
-      </div>
-    </li>
-  );
+export interface ICartItemState {
+  quantity: number;
+}
+
+class CartItem extends React.Component<ICartItemProps, ICartItemState> {
+  state = {
+    quantity: this.props.quantity
+  };
+
+  buildSelectOptions = () => {
+    var arr = [];
+    for (let i = 1; i <= 30; i++) {
+      arr.push(
+        <option key={i} value={i}>
+          {i}개
+        </option>
+      );
+    }
+    return arr;
+  };
+
+  handleQuantityChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    // DB에 수량 갱신도 필요.
+    // 갱신하고 장바구니 DB 다시 불러오는 것도 필요.
+    // 장바구니 DB 다시 불러오지 않으려면 Redux에 있는 Cart 데이터 수정하는 함수 필요할듯?
+    this.setState({
+      quantity: Number(e.currentTarget.value)
+      // option의 value가 string으로 들어가서 Number로 변환해줘야 하는듯.
+    });
+  };
+
+  render() {
+    const { quantity } = this.state;
+    const { name, price, option } = this.props;
+    const { buildSelectOptions, handleQuantityChange } = this;
+
+    return (
+      <li className="cart__item">
+        <img src="/img/coffee-sample.png" alt="Coffee Sample" className="cart__item-img" />
+        <div className="cart__item-info">
+          <span className="cart__item-name">
+            {name} - {numberCommaRegex(price)}원
+          </span>
+          <span className="cart__item-option">{option}</span>
+        </div>
+        <div className="cart__item-quantity">
+          <select
+            className="cart__item-quantity-select"
+            name="quantity"
+            value={quantity}
+            onChange={e => handleQuantityChange(e)}
+          >
+            {buildSelectOptions()}
+          </select>
+        </div>
+      </li>
+    );
+  }
 }
 
 export default CartItem;
