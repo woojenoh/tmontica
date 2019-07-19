b2d4ff0fd5b2f523b75a1dc019004c3a264fe6ba
WOOJE <woojenoh@gmail.com>
2019-07-19 18:27:36 +0900

    OrderList 더미 데이터 적용

--- front-end/src/service/components/OrderList/index.tsx
+++ front-end/src/service/components/OrderList/index.tsx
@@ -7,13 +7,46 @@ export interface IOrderListProps {}
 export interface IOrderListState {}
 
 class OrderList extends React.Component<IOrderListProps, IOrderListState> {
+  tempData = {
+    orders: [
+      {
+        orderId: 12,
+        orderDate: "2019-07-19 11:12:40",
+        status: "결제완료",
+        menuNames: ["청포도에이드", "카페라떼", "소보로빵"]
+      },
+      {
+        orderId: 11,
+        orderDate: "2019-07-19 11:12:40",
+        status: "준비완료",
+        menuNames: ["아메리카노", "카페라떼", "소보로빵"]
+      },
+      {
+        orderId: 10,
+        orderDate: "2019-07-19 11:12:40",
+        status: "픽업완료",
+        menuNames: ["카페모카", "카페라떼", "소보로빵"]
+      }
+    ]
+  };
+
   public render() {
+    const { tempData } = this;
+
     return (
       <div className="orders-list__content">
         <ul className="orders-list__items">
-          <OrderListItem />
-          <OrderListItem />
-          <OrderListItem />
+          {tempData.orders.map(o => {
+            return (
+              <OrderListItem
+                key={o.orderId}
+                orderId={o.orderId}
+                name={`${o.menuNames[0]} 외 ${o.menuNames.length - 1}개`}
+                date={o.orderDate}
+                status={o.status}
+              />
+            );
+          })}
         </ul>
       </div>
     );
--- front-end/src/service/components/OrderListItem/index.tsx
+++ front-end/src/service/components/OrderListItem/index.tsx
@@ -1,18 +1,33 @@
 import * as React from "react";
 import "./styles.scss";
 
-export interface IOrderListItemProps {}
+export interface IOrderListItemProps {
+  orderId: number;
+  name: string;
+  date: string;
+  status: string;
+}
 
 function OrderListItem(props: IOrderListItemProps) {
+  const { orderId, name, date, status } = props;
+
   return (
-    <li className="orders-list__item">
+    <li
+      className={
+        status === "준비완료"
+          ? "orders-list__item orders-list__item--ready"
+          : status === "픽업완료"
+          ? "orders-list__item orders-list__item--pickuped"
+          : "orders-list__item"
+      }
+    >
       <div className="orders-list__item-left">
-        <span className="orders-list__item-number">00001</span>
-        <span className="orders-list__item-title">아메리카노 외 1개</span>
+        <span className="orders-list__item-number">{orderId}</span>
+        <span className="orders-list__item-names">{name}</span>
       </div>
       <div className="orders-list__item-right">
-        <span className="orders-list__item-date">2019-07-09 11:12:40</span>
-        <span className="orders-list__item-status">미결제</span>
+        <span className="orders-list__item-date">{date}</span>
+        <span className="orders-list__item-status">{status}</span>
       </div>
     </li>
   );
