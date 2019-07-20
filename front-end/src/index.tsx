965441f18f354e89e1e2124e224a863e083a4b03
이준열 <danjun@tmon.co.kr>
2019-07-20 19:45:37 +0900

    underscore 랴이브러리 추가

--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -5,6 +5,7 @@ import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 import "./assets/scss/service.scss";
 import "./assets/scss/reset.scss";
+import _ from "underscore";
 
 ReactDOM.render(
   <BrowserRouter>
