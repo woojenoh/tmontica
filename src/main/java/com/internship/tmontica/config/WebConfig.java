f9bb6c61964bb78ffd4c2df60037ea44a68f2271
김형석 <vndtjd1217@naver.com>
2019-07-23 16:00:22 +0900

    fix webconfig

--- src/main/java/com/internship/tmontica/config/WebConfig.java
+++ src/main/java/com/internship/tmontica/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.internship.tmontica.config;
 
 import com.internship.tmontica.security.JwtInterceptor;
 import com.internship.tmontica.security.JwtService;
+import com.internship.tmontica.util.UserConfigValueName;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -25,7 +26,8 @@ public class WebConfig implements WebMvcConfigurer {
         // 모든 uri에 대해 http://localhost:18080, http://localhost:8180 도메인은 접근을 허용한다.
         registry.addMapping("/**")
                 .allowedOrigins("*") //http://localhost:3000
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders(UserConfigValueName.JWT_TOKEN_HEADER_KEY);
     }
 
     @Override
