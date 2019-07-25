d733b39985839b2ff24d1c458a7ba965d109aa54
이준열 <danjun@tmon.co.kr>
2019-07-25 11:27:43 +0900

    [MenuDetail] 바로구매 withJWT

--- front-end/src/API.ts
+++ front-end/src/API.ts
@@ -3,8 +3,8 @@ import history from "./history";
 
 // const API_URL = "http://localhost:3000/fakeapi";
 // const API_URL = "https://my-json-server.typicode.com/yeolsa/tmontica-json";
-// const API_URL = "http://tmontica-idev.tmon.co.kr/api";
-export const API_URL = "http://localhost:8080/api";
+export const API_URL = "http://tmontica-idev.tmon.co.kr/api";
+// export const API_URL = "http://localhost:8080/api";
 
 function fetchJSON(reqURL: string) {
   return fetch(reqURL, {
@@ -16,11 +16,14 @@ function fetchJSON(reqURL: string) {
   });
 }
 
-function post(reqURL: string, data: any) {
+function postWithJWT(reqURL: string, data: any) {
+  const jwt = localStorage.getItem("JWT") || "";
+
   return fetch(reqURL, {
     headers: {
       Accept: "application/json",
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
+      Authorization: jwt
     },
     method: "POST",
     body: JSON.stringify(data)
@@ -52,7 +55,7 @@ export const MenuAPI = (() => {
 export const CartAPI = (() => {
   function* addCart(cartAddReqs: Array<TCartAddReq>) {
     try {
-      yield post(`${API_URL}/carts`, cartAddReqs);
+      yield postWithJWT(`${API_URL}/carts`, cartAddReqs);
       if (cartAddReqs[0].direct) {
         localStorage.setItem("isDirect", "Y");
         history.push("/payment");
