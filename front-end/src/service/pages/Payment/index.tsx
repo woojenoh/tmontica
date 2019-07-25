cc907147e47c2b967d0052127b29fcde6532a652
노우제 <woojenoh@gmail.com>
2019-07-25 17:57:08 +0900

    CSS 일부 수정

--- front-end/src/service/pages/Payment/index.tsx
+++ front-end/src/service/pages/Payment/index.tsx
@@ -71,48 +71,61 @@ export default class Payment extends React.PureComponent<IPaymentProps, IPayment
     return (
       <>
         <main className="main">
-          <section className="order card">
-            <div className="order__top">
-              <h2 className="order__title plr15">주문상품정보</h2>
-            </div>
-            <div className="order__main">
-              <ul className="order__menus">
-                {orderCarts.map((oc: ICartMenu) => {
-                  return (
-                    <OrderMenu
-                      key={oc.cartId}
-                      nameKo={oc.nameKo}
-                      quantity={oc.quantity}
-                      imgUrl={oc.imgUrl}
-                      price={oc.price}
-                      option={oc.option}
-                    />
-                  );
-                })}
-              </ul>
-            </div>
-          </section>
-          <section className="payment card">
-            <div className="payment__top">
-              <h2 className="payment__title plr15">결제방법</h2>
-            </div>
-            <div className="payment__methods button--group">
-              <div className="button button--green payment__method active">현장결제</div>
-              <div className="button button--green payment__method">카드</div>
-            </div>
-            <div className="payment__points">
-              <input
-                type="text"
-                name="point"
-                value={this.state.usedPoint}
-                onChange={e => {
-                  let willUsedPoint = parseInt(e.currentTarget.value) || 0;
+          <div className="order__container">
+            <section className="order card">
+              <div className="order__top">
+                <h2 className="order__title plr15">주문상품정보</h2>
+              </div>
+              <div className="order__main">
+                <ul className="order__menus">
+                  {orderCarts.map((oc: ICartMenu) => {
+                    return (
+                      <OrderMenu
+                        key={oc.cartId}
+                        nameKo={oc.nameKo}
+                        quantity={oc.quantity}
+                        imgUrl={oc.imgUrl}
+                        price={oc.price}
+                        option={oc.option}
+                      />
+                    );
+                  })}
+                </ul>
+              </div>
+            </section>
+            <section className="payment card">
+              <div className="payment__top">
+                <h2 className="payment__title plr15">결제방법</h2>
+              </div>
+              <div className="payment__methods button--group">
+                <div className="button button--green payment__method active">현장결제</div>
+                <div className="button button--green payment__method">카드</div>
+              </div>
+              <div className="payment__points">
+                <input
+                  type="text"
+                  name="point"
+                  value={this.state.usedPoint}
+                  onChange={e => {
+                    let willUsedPoint = parseInt(e.currentTarget.value) || 0;
 
-                  if (orderPrice - willUsedPoint < 0) {
-                    alert("주문금액 보다 많이 입력할 수 없습니다.");
+                    if (orderPrice - willUsedPoint < 0) {
+                      alert("주문금액 보다 많이 입력할 수 없습니다.");
+                      this.setState(
+                        {
+                          usedPoint: 0
+                        },
+                        () => {
+                          this.setState({
+                            totalPrice: orderPrice - this.state.usedPoint
+                          });
+                        }
+                      );
+                      return;
+                    }
                     this.setState(
                       {
-                        usedPoint: 0
+                        usedPoint: willUsedPoint
                       },
                       () => {
                         this.setState({
@@ -120,67 +133,54 @@ export default class Payment extends React.PureComponent<IPaymentProps, IPayment
                         });
                       }
                     );
-                    return;
-                  }
-                  this.setState(
-                    {
-                      usedPoint: willUsedPoint
-                    },
-                    () => {
-                      this.setState({
-                        totalPrice: orderPrice - this.state.usedPoint
-                      });
-                    }
-                  );
-                }}
-              />
-              <div>
-                사용가능한 포인트: <span>0</span>P
+                  }}
+                />
+                <div>
+                  사용가능한 포인트: <span>0</span>P
+                </div>
               </div>
-            </div>
-          </section>
+            </section>
 
-          <section className="price card">
-            <div className="d-flex">
-              <div className="price--name">주문금액</div>
-              <div className="price--value">{orderPrice.toLocaleString()}원</div>
-            </div>
-            <div className="d-flex">
-              <div className="price--name">할인금액</div>
-              <div className="price--value">{this.state.usedPoint.toLocaleString()}원</div>
-            </div>
-            <hr />
-            <div className="d-flex">
-              <div className="price--name">최종 결제금액</div>
-              <div className="price--value">
-                {(this.state.totalPrice && this.state.totalPrice.toLocaleString()) ||
-                  orderPrice.toLocaleString()}
-                원
+            <section className="price card">
+              <div className="d-flex">
+                <div className="price--name">주문금액</div>
+                <div className="price--value">{orderPrice.toLocaleString()}원</div>
               </div>
-            </div>
-            <div className="button--group">
-              <div className="button--cancle button button--green">취소</div>
-              <div
-                className="button--pay button button--green"
-                onClick={() => {
-                  if (window.confirm("결제하시겠습니까?")) {
-                    // TODO: 결제하기 API 호출
-                    OrderAPI.order({
-                      menus: [
-                        
-                      ],
-                      usedPoint: this.state.usedPoint,
-                      totalPrice: this.state.totalPrice,
-                      payment: "현장결제"
-                    })
-                    // 내 주문 페이지로 이동
-                  }
-                }}
-              >
-                결제하기
+              <div className="d-flex">
+                <div className="price--name">할인금액</div>
+                <div className="price--value">{this.state.usedPoint.toLocaleString()}원</div>
               </div>
-            </div>
-          </section>
+              <hr />
+              <div className="d-flex">
+                <div className="price--name">최종 결제금액</div>
+                <div className="price--value">
+                  {(this.state.totalPrice && this.state.totalPrice.toLocaleString()) ||
+                    orderPrice.toLocaleString()}
+                  원
+                </div>
+              </div>
+              <div className="button--group">
+                <div className="button--cancle button button--green">취소</div>
+                <div
+                  className="button--pay button button--green"
+                  onClick={() => {
+                    if (window.confirm("결제하시겠습니까?")) {
+                      // TODO: 결제하기 API 호출
+                      OrderAPI.order({
+                        menus: [],
+                        usedPoint: this.state.usedPoint,
+                        totalPrice: this.state.totalPrice,
+                        payment: "현장결제"
+                      });
+                      // 내 주문 페이지로 이동
+                    }
+                  }}
+                >
+                  결제하기
+                </div>
+              </div>
+            </section>
+          </div>
         </main>
         <footer className="footer">
           <div className="footer__container" />
