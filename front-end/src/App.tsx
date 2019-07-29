d44ae593875c050fe026b46db2d6d975053028c5
노우제 <woojenoh@gmail.com>
2019-07-29 14:02:57 +0900

    로그인 후 로그인 페이지로 이동시 리다이렉트

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -77,9 +77,36 @@ class App extends React.Component<IAppProps> {
     }
   };
 
+  PublicRoute = ({ component: Component, ...rest }: RouteProps) => {
+    const { isSignin } = this.props;
+
+    if (Component) {
+      return (
+        <Route
+          {...rest}
+          render={props =>
+            // 로그인이 돼있는 상태에서 로그인 페이지로 이동하면 리다이렉트한다.
+            isSignin && (rest.path === "/signin" || rest.path === "/find") ? (
+              <Redirect
+                to={{
+                  pathname: "/",
+                  state: { from: props.location }
+                }}
+              />
+            ) : (
+              <Component {...props} />
+            )
+          }
+        />
+      );
+    } else {
+      return null;
+    }
+  };
+
   render() {
     const { location } = this.props;
-    const { AdminRoute, PrivateRoute } = this;
+    const { AdminRoute, PrivateRoute, PublicRoute } = this;
 
     return (
       <>
@@ -94,14 +121,14 @@ class App extends React.Component<IAppProps> {
           <PrivateRoute exact path="/payment" component={Payment} />
           <PrivateRoute exact path="/orders" component={Orders} />
           <PrivateRoute exact path="/user" component={Menus} />
-          <Route exact path="/menus/:menuId([0-9]+)" component={Menu} />
-          <Route exact path="/menus/:categoryEng([a-zA-Z]+)" component={MenusSub} />
-          <Route exact path="/admin/signin" component={Menus} />
-          <Route exact path="/signin" component={Signin} />
-          <Route exact path="/signup" component={Signup} />
-          <Route exact path="/find" component={FindAccount} />
-          <Route exact path="/" component={Menus} />
-          <Route path="*" component={Menus} />
+          <PublicRoute exact path="/menus/:menuId([0-9]+)" component={Menu} />
+          <PublicRoute exact path="/menus/:categoryEng([a-zA-Z]+)" component={MenusSub} />
+          <PublicRoute exact path="/admin/signin" component={Menus} />
+          <PublicRoute exact path="/signin" component={Signin} />
+          <PublicRoute exact path="/signup" component={Signup} />
+          <PublicRoute exact path="/find" component={FindAccount} />
+          <PublicRoute exact path="/" component={Menus} />
+          <PublicRoute path="*" component={Menus} />
         </Switch>
       </>
     );
