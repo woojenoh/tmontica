bb2717fa080802cf755e3b1f4aab8471f939c5b5
김형석 <vndtjd1217@naver.com>
2019-07-30 15:01:41 +0900

    에러포맷 통일

--- src/main/java/com/internship/tmontica/exception/TmonTicaExceptionFormat.java
+++ src/main/java/com/internship/tmontica/exception/TmonTicaExceptionFormat.java
@@ -1,15 +1,33 @@
 package com.internship.tmontica.exception;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Getter
 public class TmonTicaExceptionFormat {
 
     private String field;
     private String exceptionMessage;
+    private List<FieldError> errors;
 
     public TmonTicaExceptionFormat(String field, String exceptionMessage) {
         this.field = field;
         this.exceptionMessage = exceptionMessage;
+    }
+
+    public TmonTicaExceptionFormat(String field, String exceptionMessage, List<FieldError> errors){
+        this.field = field;
+        this.exceptionMessage = exceptionMessage;
+        this.errors = errors;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    public static class FieldError{
+        private String field;
+        private String value;
+        private String reason;
     }
 }
